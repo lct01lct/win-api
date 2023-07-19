1eb7029d470752215f19ef3f534aefdb0ad67f32
刘纯涛 <1786215856@qq.com>
2023-07-19 17:33:02 +0800

    refactor(all): delete unnecessary fields

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import * as cookieParser from 'cookie-parser';
 import morgan = require('morgan');
 import { logger } from './utils';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { VersioningType } from '@nestjs/common';
 
 (async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, AppModule.appConfig);
