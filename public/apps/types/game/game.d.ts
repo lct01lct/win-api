ddf7fd8e8e3f91a5a0f509e977b109f3972f1a65
刘纯涛 <1786215856@qq.com>
2023-07-25 16:01:17 +0800

    build(scripts): remove apps dir when building apps

--- public/apps/types/game/game.d.ts
+++
@@ -1,3 +0,0 @@
-import { AppOrigin } from 'win';
-declare const _default: AppOrigin;
-export default _default;
--- scripts/collect-app.js
+++ scripts/collect-app.js
@@ -28,6 +28,23 @@ function copyFolderRecursiveSync(source, destination) {
   });
 }
 
+const deleteFolderRecursive = function (directoryPath) {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach((file, index) => {
+      const curPath = path.join(directoryPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(directoryPath);
+  }
+};
+
+deleteFolderRecursive(path.join(__dirname, '../public/apps'));
 copyFolderRecursiveSync(
   path.join(__dirname, `../../${process.env.FRONT_END_PROJECT_NAME}/packages/app-store/dist`),
   path.join(__dirname, '../public/apps')
